536412b58783f021f9f019189407552e076a4c0c
Sansnn <905558609@qq.com>
2023-08-02 17:00:25 +0800

    选择模型

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			'/api': {
-				target: 'http://127.0.0.1:7860', // 目标服务  
+				target: 'https://service-0k7mlw02-1300605312.gz.apigw.tencentcs.com/release', // 目标服务  
 				changeOrigin: true,
 				rewrite: path => path.replace(/^\/api/, ''),
 			}
